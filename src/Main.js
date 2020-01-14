6385b4b05a23e9262b44637df2b679b70577f276
Artyom K <kezina2036@gmail.com>
2020-01-14 18:17:40 +0300

    requests has been added

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { useGet } from './requests';
+import React, { useState } from 'react';
+import { useGet, usePost } from './requests';
 
 const Main = () => {
-    const [url, setUrl] = useState('');
-    let response = useGet(url);
-    console.log(response)
+    const [paramsGet, setParamsGet] = useState({
+        url: "",
+        reload: 0,
+        options: {}
+    });
+
+    const [paramsPost, setParamsPost] = useState({
+        url: "",
+        reload: 0,
+        options: {body: {}}
+    });
+    const [inputValue, setInputValue] = useState({});
+
+
+
+    const handleGet = (e) => {
+        e.preventDefault();
+        setParamsGet({
+            ...paramsGet,
+            url: e.target.action,
+            reload: Math.random()
+        })
+    }
+
+    const handlePost = (e) => {
+        e.preventDefault();
+        setParamsPost({
+            ...paramsPost,
+            url: e.target.action,
+            reload: Math.random(),
+            options: {
+                body: JSON.stringify(inputValue)
+            }
+        })
+    }
+
+    const handleInput = (e) => {
+        setInputValue({
+            ...inputValue,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    let responseGet = useGet(paramsGet);
+    let responsePost = usePost(paramsPost);
 
     return(
-        <div className = 'general-container' >
-            <button onClick = {() => setUrl('/back')}>req</button>
+        <div className = "general-container" >
+            <form action = "/back" method = "GET" onSubmit = {handleGet}>
+                <input type = "submit" value = "GET" />
+            </form>
+            <form action = "/back" method = "POST" onSubmit = {handlePost}>
+                <input type = "text" name = "Login" onChange = {handleInput} placeholder = "Enter login" />
+                <input type = "password" name = "Password" onChange = {handleInput} placeholder = "Enter password" />
+                <input type = "submit" value = "POST" />
+            </form>
             <h1 style = {{color: "red", fontSize: "5em", backgroundColor: "yellow", position: "relative", margin: "auto", top: "10vh"}}>
-                {response}
+                {responseGet}
             </h1> 
         </div>
     )
--- src/requests.js
+++ src/requests.js
@@ -1,23 +1,74 @@
-import React, {useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 
-export const useGet = (url, dependencies = []) => {
+
+
+export const useGet = (params, dependencies = []) => {
+    const {url, reload, options} = params;
     const [loading, setLoading] = useState(false);
     const [response, setResponse] = useState(null);
 
     useEffect(() => {
         url && (async () => {
             setLoading(true);
-            setResponse(
-                await (
-                    await fetch(url)
-                ).json()
-            )
+            try {
+                setResponse(
+                    await (
+                        await fetch(url, {
+                            method: "GET",
+                            ...options
+                        })
+                    ).json()
+                )
+            } catch(e) {
+                alert("No such address");
+            }
             setLoading(false);
         })()
-    }, [url].concat(dependencies))
+    }, [reload, ...dependencies])
 
-    if(response === null){
+    console.log(response, Date.now());
+
+    if(response === null) {
         if(loading) return "loading...";
         return null;
     } return response.name;
+}
+
+
+
+export const usePost = (params, dependencies = []) => {
+    const {url, reload, options} = params;
+    const [loading, setLoading] = useState(false);
+    const [response, setResponse] = useState(null);
+
+    useEffect(() => {
+        options.body &&
+        url && (async () => {
+            setLoading(true);
+            try {
+                setResponse(
+                    await(
+                        await fetch(url, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json;charset=utf-8",
+                            },
+                            ...options
+                        })
+                    ).json()
+                )
+                
+            } catch(e) {
+                alert("Try again");
+            }
+            setLoading(false);
+        })()
+    }, [reload, ...dependencies])
+
+    console.log(response, Date.now())
+
+    if(response === null) {
+        if(loading) return "loading...";
+        return null;
+    } return response;
 }
